d582cb3d7fe7e9db7c2cbe20b5434c66e3fae955
SFUMisha <mea28@sfu.ca>
2025-04-16 21:22:21 -0700

    added delay after pressing button

--- launch/js/main.js
+++ launch/js/main.js
@@ -22,6 +22,7 @@ function isValidEmail(email) {
 }
 
 async function preOrder() {
+    await wait(300);
     const buttonContainer = document.querySelector(".button-container");
     if (buttonContainer) {
         buttonContainer.remove();
