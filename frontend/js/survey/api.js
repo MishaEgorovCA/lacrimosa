4e5ce37b8cbc0b9ec9682ca395fdb499d48e91a9
SFUMisha <mea28@sfu.ca>
2025-08-27 17:43:32 -0700

    fixed api call paths and changed access.html to index.

--- frontend/js/survey/api.js
+++ frontend/js/survey/api.js
@@ -1,5 +1,5 @@
 export async function getIntro(code) {
-        const res = await fetch("/get-message", {
+        const res = await fetch("/api/get-message", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ code })
